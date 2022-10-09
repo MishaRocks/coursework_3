0102bf35b9bc34a1a729387d14437ab5046f1df9
MishaRocks <tehobox@yandex.ru>
2022-10-10 00:04:03 +0300

    После всех экспериментов

---
+++ app/newtest.py
@@ -0,0 +1,18 @@
+import os
+
+from utils import open_json_read
+from posts.posts_views import ROOT_DIR
+
+# CONFIG_PATH = os.path.join(ROOT_DIR, 'app', 'data', 'posts.json')
+
+#
+# def all_posts():
+#
+#     file = CONFIG_PATH
+#     data = open_json_read(file)
+#
+#
+#     return data
+
+print(ROOT_DIR)
+
